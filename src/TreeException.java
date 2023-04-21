a2c04084ba1c7296e1a0d8d2e08e07ed96dc26a1
Gloria <glorialiwang333@gmail.com>
2023-04-21 09:14:03 -0700

    ...

--- src/TreeException.java
+++ src/TreeException.java
@@ -1,7 +1,9 @@
-public class TreeException extends FamilyTree
+public class TreeException extends Exception
 {
 	public TreeException(String s)
 	{
 		// Pass s into a superclass ctor.
+		super(s);
+
 	}
 }
